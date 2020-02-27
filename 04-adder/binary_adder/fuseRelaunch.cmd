0e3738a7502e09c73ab6d137f62feedad5b2a8dd
lab661 <lab661@PC-453.urel.feec.vutbr.cz>
2020-02-27 10:01:43 +0100

    adding 04 lab

---
+++ 04-adder/binary_adder/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/lab661/Documents/marak/Digital-electronics-1/04-adder/binary_adder/top_tb00_isim_beh.exe" -prj "/home/lab661/Documents/marak/Digital-electronics-1/04-adder/binary_adder/top_tb00_beh.prj" "work.top_tb00" 
