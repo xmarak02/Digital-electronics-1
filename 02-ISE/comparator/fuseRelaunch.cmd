62f35064e115f64283f7dceae7416d01953813c3
xmarak02 <xmarak02@vutbr.cz>
2021-02-10 14:23:21 +0100

    Creating 01-gates lab

--- 02-ISE/comparator/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "/home/lab661/Documents/marak/Digital-electronics-1/02-ISE/comparator/top_tb00_isim_beh.exe" -prj "/home/lab661/Documents/marak/Digital-electronics-1/02-ISE/comparator/top_tb00_beh.prj" "work.top_tb00" 
